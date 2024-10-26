f1f376f01880b1c4baafaff7a28ecb5bdf7b7b19
ArsTyzykhyan <ego1st.jc@gmail.com>
2024-10-26 11:50:26 +0300

    the "for" loop

---
+++ Practice.py
@@ -0,0 +1,20 @@
+   # your_wallet = int(input("Введите состояние своего банковского счета: "))
+    #course_price = 75000
+    #if your_wallet >= course_price:
+     #   var = your_wallet - course_price
+      #  print("Курс успешно приобретён,остаток на вашем счете: ",var)
+    #else:
+     #   print("Недостаточно денег")
+    #print("Хорошего дня!")
+
+#while input != 5:
+   # sons_number = int(input("Введите число от 1 до 10: "))
+   # father_number = 5
+    #if sons_number != father_number:
+     #   print("Вы проиграли!")
+     #   continue
+    #else:
+    #    print("Вы победили,поздравляем!")
+    #    break
+#print("Конец игры!")
+
