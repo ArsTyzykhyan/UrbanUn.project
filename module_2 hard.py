1fe22a869c2fc48bb6a2d1b282cd929f9fa9e1eb
ArsTyzykhyan <ego1st.jc@gmail.com>
2024-10-28 19:06:38 +0300

    functions!

--- module_2 hard.py
+++ module_2 hard.py
@@ -1,21 +1,11 @@
-def generate_password(n):
-    """Generates a password based on the given number n.
-
-    Args:
-        n: An integer between 3 and 20.
-
-    Returns:
-        A string representing the password.
-    """
-    password = ""
+def ancient_cipher(n):
+    result = ""
     for i in range(1, n):
         for j in range(i + 1, n + 1):
-            if (i + j) % n == 0:
-                password += str(i) + str(j)
-    return password
+            if n % (i + j) == 0:
+                result += str(i) + str(j)
+    return result
 
-n = int(input("Enter a number between 3 and 20: "))
-if 3 <= n <= 20:
-    print("Password:", generate_password(n))
-else:
-    print("Invalid input. Please enter a number between 3 and 20.")
+n = int(input("Введите число от 3 до 20: "))
+result = ancient_cipher(n)
+print(f"Пароль для числа {n}: {result}")
