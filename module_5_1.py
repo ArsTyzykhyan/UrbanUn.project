557f82fef6fd3a0dc8a6d2f40faa63ab55f0af29
ArsTyzykhyan <ego1st.jc@gmail.com>
2024-11-22 21:33:40 +0300

    class_attributes and methods(1.0)

--- module_5_1.py
+++ module_5_1.py
@@ -7,12 +7,12 @@ class House:
         if 1 < new_floor < self.number_of_floors:
             for i in range(1, new_floor + 1):
                 print(i)
-            else:
-                print('Такого этажа не существует')
+        else:
+            print('Такого этажа не существует')
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
-h2.go_to(10)
+h2.go_to(-1)
 
 
